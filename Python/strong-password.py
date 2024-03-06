bda847e2a353818e7a5262b07a507410c0a5a3b5
Enzzx <101784007+Enzzx@users.noreply.github.com>
2024-03-06 07:31:43 -0300

    criptografia de senha

---
+++ Python/strong-password.py
@@ -0,0 +1,10 @@
+rValues = ['b', '4', 'g', 'h', 'a', 'i', '6', 'r', 'd', 'k', 'e', '7', 'j', '5', 'n', 'f', 'c', '9', '0', 't', 'x', '3', 'q', 'm', 'z', '2', 'l', 'o', '8', 's', 'u', 'v', 'p', 'w', 'y', 'l']
+
+""" print("informe a chave:")
+listKey = list(input())
+key = []
+for item in listKey:
+    key.append(rValues.index(item))
+
+print(key) """
+
