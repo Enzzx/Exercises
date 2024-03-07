95236f47ade027da7cdc49597a1001b1db521baa
Enzzx <101784007+Enzzx@users.noreply.github.com>
2024-03-07 17:06:25 -0300

    invertendo binários

--- Python/cryptography.py
+++ Python/cryptography.py
@@ -1,29 +1,39 @@
 rValues = ['b', '4', 'g', 'h', 'a', 'i', '6', 'r', 'd', 'k', 'e', '7', 'j', '5', " ", 'n', 'f', 'c', '9', '0', 't', 'x', '3', 'q', 'm', 'z', '2', 'l', 'o', "ç", '8', 's', 'u', 'v', 'p', 'w', 'y', 'l']
 
-""" print("informe a chave:")
-listKey = list(input())
-key = []
-for item in listKey:
-    key.append(rValues.index(item))
 
-print(key) """
+def cryptograph():
+    print("informe a chave:")
+    listKey = list(input())
+    key = []
+    for item in listKey:
+        key.append(item)
+    print(key)
 
-chave = ["c", "h", "a", "v", "e", "z", "o", "n", "a"]
-senha = ["a", "b", "c", "d", "e", "f", "g", "h"]
-package = senha[:4]
-senha = senha[4:]
+    print("informe a senha:")
+    listPass = list(input())
+    psswrd = []
+    for psw in listPass:
+        psswrd.append(psw)
+    print(psswrd)
 
-chave_indices = [rValues.index(value) for value in chave]
-package_indices = [rValues.index(value) for value in package]
+    package = psswrd[:4]
+    psswrd = psswrd[4:]
 
-i = 0
-for indice in chave_indices:
-    package_indices[i] += indice
-    if i >= len(package_indices) -1:
-        i = 0
-    i+=1
+    key_indices = [rValues.index(value) for value in key]
+    package_indices = [rValues.index(value) for value in package]
 
+    i  = 0
+    for tooth in key_indices:
+        package_indices[i % len(package_indices)] += tooth
+        i+=1
 
-print(package_indices)
-print(chave_indices)
-print(package_sum)
+    i = 0
+    for packNum in package_indices:
+        binary = bin(packNum)
+        binary = binary[2:]
+        binary = binary[len(binary)//2:]+binary[0:len(binary)//2]
+        package_indices[i] = binary
+        i+=1
+    print(package_indices)
+
+cryptograph()
