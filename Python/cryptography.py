6e137e5663748a9961f724e49403be0b4c9dd96e
Enzzx <101784007+Enzzx@users.noreply.github.com>
2024-03-08 08:10:20 -0300

    tá indo

--- Python/cryptography.py
+++ Python/cryptography.py
@@ -15,25 +15,30 @@ def cryptograph():
     for psw in listPass:
         psswrd.append(psw)
     print(psswrd)
+    
+    encripted = []
 
-    package = psswrd[:4]
-    psswrd = psswrd[4:]
-
-    key_indices = [rValues.index(value) for value in key]
-    package_indices = [rValues.index(value) for value in package]
-
-    i  = 0
-    for tooth in key_indices:
-        package_indices[i % len(package_indices)] += tooth
-        i+=1
-
-    i = 0
-    for packNum in package_indices:
-        binary = bin(packNum)
-        binary = binary[2:]
-        binary = binary[len(binary)//2:]+binary[0:len(binary)//2]
-        package_indices[i] = binary
-        i+=1
-    print(package_indices)
-
+    while len(psswrd) > 0:
+        package = psswrd[:4]
+        psswrd = psswrd[4:]
+    
+        key_indices = [rValues.index(value) for value in key]
+        package_indices = [rValues.index(value) for value in package]
+    
+        i  = 0
+        for tooth in key_indices:
+            package_indices[i % len(package_indices)] += tooth
+            i+=1
+    
+        i = 0
+        for packNum in package_indices:
+            binary = bin(packNum)
+            binary = binary[2:]
+            binary = binary[len(binary)//2:]+binary[0:len(binary)//2]
+            package_indices[i] = rValues[int(binary, 2)]
+            i+=1
+            print("iterou o " + str(i))
+        print(package_indices)
+        encripted.extend(package_indices)
+    print(encripted)
 cryptograph()
