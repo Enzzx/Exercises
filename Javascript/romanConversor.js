9178a70d7f5f3f9fb64b7d2f713ee19fb3a3373c
Enzzx <bellottie815@gmail.com>
2026-03-12 03:49:23 -0300

    change romanConversor logic

--- Javascript/romanConversor.js
+++ Javascript/romanConversor.js
@@ -1,73 +1,96 @@
-class ADF {
-    constructor(romanExpr = "CLXIV") {
-        this.romanDict = {
-            "M": 1000,
-            "D": 500,
-            "C": 100,
-            "L": 50,
-            "X": 10,
-            "V": 5,
-            "I": 1
-        }
-
+class AFD {
+    constructor(romanExpr) {
         this.romanExpr = romanExpr
-        this.index = 0
+        this.lastVal = ''
+        this.i = 0
         this.actVal = ''
-        this.state = "q0"
+        this.state = 'q0'
         this.arabicSum = 0
     }
 
-    nextVal() {
-        if (this.romanDict[this.romanExpr[this.index+1]] < this.romanDict[this.romanExpr[this.index+2]]) {
-            return String(this.romanExpr[++this.index], this.romanExpr[++this.index])
-        }
-        return this.romanExpr[++this.index] 
-    }
+    convert() {
+        while (this.i < this.romanExpr.length) {
+            this.lastVal = this.actVal
+            this.actVal = this.romanExpr[this.i++]
 
-    thousand() {
-        while(this.index < this.romanExpr.length) {
-            switch([this.actVal, this.statetate]) {
-                case ['', 'q0']:
-                    this.actVal = this.nextVal
-                    this.state = "q0"
+            switch(this.state) {
+                case "q0":
+                    if (this.actVal == "M") { this.arabicSum+=1000; this.state = 'q1'}
+                    else this.state = 'q3'
                     break
-                case ['M', 'q0']:
-                    this.arabicSum += this.romanDict[this.actVal]
-                    this.actVal = this.nextVal
-                    this.state = "q1"
+                case "q1":
+                    if (this.actVal == "M") { this.arabicSum+=1000; this.state = 'q2'}
+                    else this.state = 'q3'
                     break
-                case ['M', 'q1']:
-                    this.arabicSum += this.romanDict[this.actValtVal]
-                    this.actVal = this.nextVal
-                    this.state = "q2"
+                case "q2":
+                    if (this.actVal == "M") { this.arabicSum+=1000; this.state = 'q3'}
+                    else this.state = 'q3'
                     break
-                case ['M', 'q2']:
-                    this.arabicSum += this.romanDict[this.actVal]
-                    this.actVal = this.nextVal
-                    this.state = "q3"
+                case "q3":
+                    if (this.actVal == "D") { this.arabicSum+=500; this.state = 'q4' }
+                    else this.state = "q4"
                     break
-                default:
-                    hundred()
-
+                case "q4":
+                    if (this.actVal == "C") { this.arabicSum+=100; this.state = 'q5' }
+                    else this.state = 'q8'
+                    break
+                case "q5":
+                    if (this.actVal == "C") { this.arabicSum+=100; this.state = 'q6' }
+                    else if (this.actVal == "D") { this.arabicSum+=300; this.state = 'q7' }
+                    else if (this.actVal == "M") { this.arabicSum+=800; this.state = 'q7' }
+                    else this.state = 'q7'
+                    break
+                case "q6":
+                    if (this.actVal == "C") { this.arabicSum+=100; this.state = 'q7' }
+                    else this.state = "q7"
+                    break
+                case "q7":
+                    if (this.actVal == "L") { this.arabicSum+=50; this.state = 'q8' }
+                    else this.state = "q8"
+                    break
+                case "q8":
+                    if (this.actVal == "X") { this.arabicSum+=10; this.state = 'q9' }
+                    else this.state = "q11"
+                    break
+                case "q9":
+                    if (this.actVal == "X") { this.arabicSum+=10; this.state = 'q10' }
+                    else if (this.actVal == "L") { this.arabicSum+=30; this.state = 'q11' }
+                    else if (this.actVal == "C") { this.arabicSum+=80; this.state = 'q11' }
+                    else this.state = "q11"
+                    break
+                case "q10":
+                    if (this.actVal == "X") { this.arabicSum+=10; this.state = 'q11' }
+                    else this.state = "q11"
+                    break
+                case "q11":
+                    if (this.actVal == "V") { this.arabicSum+=5; this.state = 'q12' }
+                    else this.state = "q12"
+                    break
+                case "q12":
+                    if (this.actVal == "I") { this.arabicSum+=1; this.state = 'q13' }
+                    else this.state = "q15"
+                    break
+                case "q13":
+                    if (this.actVal == "I") { this.arabicSum+=1; this.state = 'q14' }
+                    else if (this.actVal == "V") { this.arabicSum+=3; this.state = 'q15' }
+                    else if (this.actVal == "X") { this.arabicSum+=8; this.state = 'q15' }
+                    else this.state = "q15"
+                    break
+                case "q14":
+                    if (this.actVal == "I") { this.arabicSum+=1; this.state = 'q15' }
+                    else this.state = "q15"
+                    break
+                case "q15":
+                    return
             }
-        }    
-    }
-
-    hundred() {
-        while(this.index < this.romanExpr.length) {
-
         }
     }
 
-    ten() {
-        while(this.index < this.romanExpr.length) {
-
-        }
-    }
-
-    unit() {
-        while(this.index < this.romanExpr.length) {
-            
-        }
+    show() {
+        console.log(`Valor em romano:\t${this.romanExpr}\nValor em arábico:\t${this.arabicSum}`)
     }
 }
+
+const value1 = new AFD("MCMLVII")
+value1.convert()
+value1.show()
